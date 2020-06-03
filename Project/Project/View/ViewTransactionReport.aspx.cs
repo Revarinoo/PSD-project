867f6134ce5fe7e6a86834f282c2194f50ff92c1
riccoccir <52000062+riccoccir@users.noreply.github.com>
2020-06-03 10:40:27 +0700

    Revert "buat report baru"
    This reverts commit dfc106f3ee514e3d2385634a5745b129e2eea2b9.

--- Project/Project/View/ViewTransactionReport.aspx.cs
+++ Project/Project/View/ViewTransactionReport.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Project.Controller;
+using Project.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,24 @@ namespace Project.View
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                User us = (User)Session["user"];
+                if(us.RoleID == 1)
+                {
+            TransactionReport report = new TransactionReport();
+            TransactionReportViewer.ReportSource = report;
+                }else
+                {
+                    Response.Redirect("Home.aspx");
+                }
+            }
+        }
 
+        private void loadReport (TransactionReport report)
+        {
+            TokobediaDataSet dataset = TransactionReportController.getTransactionReportDataSet();
+            report.SetDataSource(dataset);
         }
     }
 }
